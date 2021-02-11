c933ef4e77cfece9797f89cee3476091724630ce
99003747 <78856378+99003747@users.noreply.github.com>
2021-02-11 17:21:48 +0530

    by ravi

--- UJJWALHELLO.C
+++ UJJWALHELLO.C
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("hello! this is ujjawal WELCOME YOU IN LTTS/n");
+    printf("hello! this is ujjawal WELCOME YOU IN LTTS\n");
+    printf("hello! this is Ravi\n");
+
     return 0;
 
 }
